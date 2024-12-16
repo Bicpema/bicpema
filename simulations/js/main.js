4e006bd67e47cd2075bf835d7b320a1fe1f70a08
yoshidakenji <63270478+ohiaeni@users.noreply.github.com>
2024-12-16 21:03:37 +0900

    シミュレーションの依存npmパッケージを追加

--- simulations/js/main.js
+++ simulations/js/main.js
@@ -3,5 +3,10 @@ import "../scss/styles.scss";
 
 // Import all of Bootstrap's JS
 import "bootstrap";
+
+// Import simulation's dependencies
 import "chart.js";
 import "p5";
+import "html2canvas";
+import "mathjs";
+import "matter-js";
