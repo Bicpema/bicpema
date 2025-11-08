ec46f721ecfa5338a38ed20f6c42c05e633b6cba
ohiaeni <63270478+ohiaeni@users.noreply.github.com>
2025-11-08 11:33:39 +0900

    セロハンアプリの追加 (#172)
    * セロハンアプリの追加
    
    * refactor: Chart.jsのインポート方法を変更
    
    * refactor: Math関数をmathに統一
    
    * セロハンのシミュレーションの別バージョンを追加

--- vite/js/common.js
+++ vite/js/common.js
@@ -9,11 +9,17 @@ window.$ = window.jQuery = $;
 import "bootstrap";
 
 // Import simulation's dependencies
-import "chart.js";
+import Chart from "chart.js/auto";
+window.Chart = Chart;
 import p5 from "p5";
-// p5をグローバルモードで初期化（全シミュレーションで利用可能に）
+// p5をグローバルモードで初期化(全シミュレーションで利用可能に)
 new p5();
 import html2canvas from "html2canvas";
 window.html2canvas = html2canvas;
-import "mathjs";
+import * as math from "mathjs";
+// mathjsの必要な関数のみをMathオブジェクトに追加
+Math.sum = math.sum;
+Math.multiply = math.multiply;
+// mathをグローバルに公開
+window.math = math;
 import "matter-js";
