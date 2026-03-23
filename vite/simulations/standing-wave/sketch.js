624fd4f3a913f9f909443420cbc4474bcafcbd57
吉田賢二 <jitianxianer018@gmail.com>
2026-03-23 21:33:49 +0900

    feat: 波の反射シミュレーションを新規作成

---
+++ vite/simulations/standing-wave/sketch.js
@@ -0,0 +1,203 @@
+// ===== 図(a)：進行波の発射 → 重なり → 定常波 =====
+// 赤：右向き進行波　青：左向き進行波　緑：合成波（定常波）
+
+let margin;
+let innerW;
+let innerH; 
+let cols;
+let dx;
+
+let t = 0;
+let wavelength = 200;
+let A = 40;
+let k, omega, v;
+let running = false;
+let moveBtn, resetBtn;
+
+// 波の先端
+let rightFront = 0;
+let leftFront;
+
+function setup() {
+  createCanvas(1000,500);
+  
+  margin = 50;
+  
+  innerW = width - margin * 2;
+  innerH = height - margin * 2;
+  cols = innerW / 4;
+
+  dx = innerW / (cols);
+  k = TWO_PI / wavelength;
+  omega = TWO_PI / 120;
+  v = omega / k;
+  leftFront = innerW;
+
+  moveBtn = createButton("スタート");
+  moveBtn.position(width/2-100, height + 10);
+  moveBtn.size(96,48);
+  moveBtn.style("background-color", "#03A9F4");
+  moveBtn.style("font", "20px");  
+  moveBtn.style("font-weight", "bold");  
+  moveBtn.style("border-radius", "5px");
+  moveBtn.style("border", "none");
+  moveBtn.style("color", "#fff");  
+  moveBtn.mousePressed(toggleMove);
+  
+  resetBtn = createButton("リセット");
+  resetBtn.position(width/2+4, height + 10);
+  resetBtn.size(96,48);
+  resetBtn.style("background-color", "#9E9E9E");
+  resetBtn.style("font-weight", "bold");  
+  resetBtn.style("color", "#fff");  
+  resetBtn.style("border-radius", "5px");
+  resetBtn.style("border", "none");
+  resetBtn.mousePressed(() => {
+    t = 0;
+    rightFront = 0;
+    leftFront = innerW;
+    running = false;
+  });
+  
+}
+  
+function toggleMove() {
+  running = !running;
+  if(running == false){
+    moveBtn.html("スタート"); 
+    moveBtn.style("background-color",  "#03A9F4");
+  }if(running == true){
+    moveBtn.html("ストップ");
+    moveBtn.style("background-color", "#F44336");
+  }
+}
+
+function draw() {
+  background(211,237,244);
+  
+  push();
+  translate(margin, margin);
+  noStroke();
+  rect(0,0,innerW,innerH);
+
+  // コンテンツの描画
+  drawGrid();
+  drawXAxis();
+  drawRightWave();
+  drawLeftWave();
+  drawStandingWave();
+  pop();
+  
+  if (running) {
+    t += v;
+    rightFront = min(v*t, innerW);
+    leftFront = max(innerW-v*t, 0);
+  }
+}
+
+// ===== グリッド描画 =====
+function drawGrid() {
+  stroke(200);
+  strokeWeight(1);
+  
+  // --- 横グリッド ---
+  let yCenter = innerH / 2;
+  let gridUnitY = wavelength / 8;
+
+  for (let y = yCenter;
+       y <= innerH;
+       y += gridUnitY) {
+    line(0, y, innerW, y);
+  }
+   for (let y = yCenter;
+       y >= 0;
+       y -= gridUnitY) {
+    line(0, y, innerW, y);
+  }
+  
+  // --- 縦グリッド ---
+  let xCenter = innerW / 2;
+  let gridUnitX = wavelength/ 8;
+
+  for (let x = xCenter;
+       x <= innerW;
+       x += gridUnitX) {
+    line(x, 0, x, innerH);
+  }
+   for (let x = xCenter;
+       x >= 0;
+       x -= gridUnitX) {
+    line(x, 0, x, innerH);
+  }
+}
+
+// ===== 横軸（x軸）＋矢印 =====
+function drawXAxis() {
+  let yAxis = innerH / 2;
+  
+  // 横軸
+  stroke(0);
+  strokeWeight(2);
+  line(0, yAxis, innerW-1, yAxis);
+
+  // 矢印
+  strokeWeight(1);
+  fill(0);
+  triangle(innerW - 10, yAxis - 5, innerW - 10, yAxis + 5, innerW, yAxis);
+
+  // 軸ラベル
+  noStroke();
+  fill(0);
+  textSize(14);
+  textAlign(RIGHT, BOTTOM);
+  text("x", innerW - 5, yAxis + 20);
+}
+
+// 右向き進行波
+function drawRightWave(){
+  stroke(255,0,0); 
+  strokeWeight(2);
+  noFill();
+  beginShape();
+  for(let x=0;x<innerW;x++){
+    if(x<rightFront){
+      let y = A*sin(k*x - omega*t);
+      vertex(x,innerH/2+y);
+  } 
+  }
+  endShape();
+}
+
+// 左向き進行波
+function drawLeftWave(){
+  stroke(0,0,255);
+  strokeWeight(2);  
+  noFill();
+  beginShape();
+  for(let x=0;x<innerW;x++){
+    if(x>leftFront){
+      let y = A*sin(k*x + omega*t);
+      vertex(x,innerH/2+y);
+    }  
+  }
+  
+  endShape();
+}
+
+// 合成波（定常波）
+function drawStandingWave(){
+  stroke(0,180,0);
+  strokeWeight(2);
+  noFill();
+
+  beginShape();
+  for(let x=0;x<innerW;x++){
+    if (x <= rightFront && x >= leftFront) {
+      let y1 = A*sin(k*x - omega*t);
+      let y2 = A*sin(k*(innerW-x) - omega*t);
+      let y = y1 + y2;
+      vertex(x,innerH/2+y);
+    }
+  }
+  endShape();
+}
---
+++ vite/simulations/tate-yoko-wave/sketch.js
@@ -0,0 +1,211 @@
+let particles = [];
+let N = 80;
+let A = 40;
+let lambda = 200;
+let k;
+let omega = 0.1;
+let t = 0;
+let moveBtn,resetBtn;
+let running = false;
+let times;
+
+let focusIndex;
+let xStart = 60;   // 波の出発点（左端）
+
+function setup() {
+  createCanvas(windowWidth, 600);
+  k = TWO_PI / lambda;
+
+  particles = [];
+  for (let i = 0; i < N; i++) {
+    let x0 = map(i, 0, N - 1, xStart, width - 60);
+    particles.push({ x0 });
+  }
+
+  focusIndex = floor(N / 2);
+
+  moveBtn = createButton("スタート");
+  moveBtn.position(width/2-100, height -60);
+  moveBtn.size(96,48);
+  moveBtn.style("background-color", "#03A9F4");
+  moveBtn.style("font", "20px");  
+  moveBtn.style("font-weight", "bold");  
+  moveBtn.style("border-radius", "5px");
+  moveBtn.style("border", "none");
+  moveBtn.style("color", "#fff");  
+  moveBtn.mousePressed(toggleMove);
+  
+  resetBtn = createButton("リセット");
+  resetBtn.position(width/2+4, height - 60);
+  resetBtn.size(96,48);
+  resetBtn.style("background-color", "#9E9E9E");
+  resetBtn.style("font-weight", "bold");  
+  resetBtn.style("color", "#fff");  
+  resetBtn.style("border-radius", "5px");
+  resetBtn.style("border", "none");
+  resetBtn.mousePressed(() => {
+    t = 0;
+    running = false;
+  });
+
+  times = createSlider(10, 60, 30, 1);
+  times.position(width/2-250, height-45);
+  times.size(120);
+}
+
+function toggleMove() {
+  running = !running;
+  if(running == false){
+    moveBtn.html("スタート"); 
+    moveBtn.style("background-color",  "#03A9F4");
+  }if(running == true){
+    moveBtn.html("ストップ");
+    moveBtn.style("background-color", "#F44336");
+  }
+}
+
+function draw() {
+  frameRate(times.value());
+  background(255);
+  if (running) t += 1;
+
+  drawLongitudinal();
+  drawConvertedTransverse();
+}
+
+// 変位
+function displacement(x0) {
+  let v = omega / k;                    // 波の速さ
+  let arrivalTime = (x0 - xStart) / v;
+
+  if (t <= arrivalTime) {
+    return 0;
+  } else {
+    // k*(x0 - xStart) とすることで、波の出発点(xStart)での位相を基準にします
+    // ここではサイン波のマイナス（変位の方向合わせ）を採用しています
+    return -A * sin(k * (x0 - xStart) - omega * t);
+  }
+}
+
+// 縦波
+function drawLongitudinal() {
+  push();
+  translate(0, height / 3);
+
+  drawAxis("縦波");
+
+  for (let p of particles) {
+    let dx = displacement(p.x0);
+    let x = p.x0 + dx;
+
+    stroke(180);
+    line(x, -50, x, 50);
+    fill(255, 0, 0);
+    noStroke();
+    circle(x, 0, 5);
+  }
+
+  // 注目粒子
+  let p = particles[focusIndex];
+  let dx = displacement(p.x0);
+  let xNow = p.x0 + dx;
+
+  fill(0, 100, 255);
+  circle(p.x0, 0, 8); // 平衡点（青）
+
+  fill(255, 0, 0);
+  circle(xNow, 0, 8); // 現在位置（赤）
+
+  // --- 修正箇所：波が到達したら矢印を描画 ---
+  let v = omega / k;
+  let arrivalTime = (p.x0 - xStart) / v;
+  // 現在時刻 t が到達時刻 arrivalTime を超えていたら矢印を描く
+  if (t > arrivalTime) {
+    drawArrow(p.x0, 0, xNow, 0);
+  }
+  // -------------------------------------------
+
+  pop();
+}
+
+// 横波
+function drawConvertedTransverse() {
+  push();
+  translate(0, height * 2 / 3);
+
+  drawAxis("横波");
+
+  // 波の線自体を細かく描写
+  noFill();
+  stroke(255,0,0);
+  strokeWeight(1);
+  beginShape();
+  for (let x = xStart; x < width - 60; x ++) {
+    let dy = displacement(x);
+    // 縦波の変位 dx を横波の変位 dy として上方向（マイナスY方向）にプロット
+    vertex(x, -dy);
+  }
+  endShape();
+
+  // 媒質の点
+  for (let p of particles) {
+    let dy = displacement(p.x0);
+    fill(255, 0, 0);
+    noStroke();
+    circle(p.x0, -dy, 5);
+    stroke(255,0,0,100);
+    line(p.x0, 0, p.x0, -dy)
+  }
+
+  // 注目粒子
+  let p = particles[focusIndex];
+  let dy = displacement(p.x0);
+
+  noStroke();
+  fill(0, 100, 255);
+  circle(p.x0, 0, 8); // 平衡点（青）
+  fill(255, 0, 0);
+  circle(p.x0, -dy, 8); // 現在位置（赤）
+  
+
+  // --- 修正箇所：波が到達したら矢印を描画 ---
+  let v = omega / k;
+  let arrivalTime = (p.x0 - xStart) / v;
+  // 現在時刻 t が到達時刻 arrivalTime を超えていたら矢印を描く
+  if (t > arrivalTime) {
+    drawArrow(p.x0, 0, p.x0, -dy);
+  }
+  // -------------------------------------------
+
+  pop();
+}
+
+function drawArrow(x1, y1, x2, y2) {
+  // 始点と終点がほぼ同じ場合は描画しない（ちらつき防止）
+  if (dist(x1, y1, x2, y2) < 1) return;
+
+  stroke(0, 200, 0);
+  strokeWeight(2);
+  line(x1, y1, x2, y2);
+  let angle = atan2(y2 - y1, x2 - x1);
+  let s = 8; // 矢印の頭のサイズ
+  push();
+  translate(x2, y2);
+  rotate(angle);
+  fill(0, 200, 0);
+  noStroke();
+  triangle(0, 0, -s, s / 2, -s, -s / 2);
+  pop();
+}
+
+function drawAxis(title) {
+  stroke(0);
+  strokeWeight(1);
+  line(50, 0, width - 50, 0);
+  fill(0);
+  triangle(width - 50, 0, width - 60, -4, width - 60, 4);
+  noStroke();
+  textSize(20);
+  textAlign(LEFT, BOTTOM);
+  text(title, 60, -60);
+}
