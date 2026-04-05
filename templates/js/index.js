b28bde44626af92c22e66293a230319913caa382
ohiaeni <63270478+ohiaeni@users.noreply.github.com>
2026-04-05 16:44:15 +0900

    シミュレーションのテンプレートをブラッシュアップ (#335)
    * テンプレートの重複を削除
    
    * docs: p5.jsシミュレーションのテンプレートパスを修正
    
    * テンプレートをブラッシュアップ
    
    * fix: テンプレートパスを修正

--- templates/js/index.js
+++ templates/js/index.js
@@ -3,7 +3,7 @@
 import p5 from "p5";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
-import { BicpemaCanvasController } from "./class.js";
+import { BicpemaCanvasController } from "./bicpema-canvas-controller.js";
 import {
   FPS,
   settingInit,
@@ -11,21 +11,14 @@ import {
   elementPositionInit,
   valueInit,
 } from "./init.js";
-// import { ... } from "./logic.js";
-// import { ... } from "./elementFunction.js";
-
-/////////////////////////// 以上の記述は不必要であれば削除してください。/////////////////////////////////
 
 const sketch = (p) => {
   const canvasController = new BicpemaCanvasController(true, false, 1.0, 1.0);
 
-  // preload 関数が必要な場合は以下のコメントを外してください。
   // p.preload = () => {
   //   font = p.loadFont("...");
   // };
 
-  // setup関数
-  // シミュレーションを実行する際に１度だけ呼び出される。
   p.setup = () => {
     canvasController.fullScreen(p);
     settingInit(p);
@@ -34,16 +27,12 @@ const sketch = (p) => {
     valueInit(p);
   };
 
-  // draw関数
-  // シミュレーションを実行した後、繰り返し呼び出され続ける
   p.draw = () => {
     p.scale(p.width / 1000);
     p.background(0);
     // drawGraph(p);
   };
 
-  // windowResized関数
-  // シミュレーションを利用しているデバイスの画面サイズが変わった際に呼び出される。
   p.windowResized = () => {
     canvasController.resizeScreen(p);
     elementPositionInit(p);
---
+++ templates/js/logic.js
@@ -0,0 +1,6 @@
+// logic.jsはシミュレーションの中心的なロジックを定義するファイルです。
+
+// メソッドの定義方法の例
+// export function exampleMethod(p) {
+//   console.log("これは例です。");
+// }
